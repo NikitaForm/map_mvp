160c45dfddd4bce0b278d29085bc41c041fe4c7f
msmoliakov <msmoliakov@jetsmarter.com>
2025-10-08 14:48:14 +0300

    delete dist

--- src/Map.tsx
+++ src/Map.tsx
@@ -5,6 +5,15 @@ import 'leaflet/dist/leaflet.css';
 import { Trash2, Undo, Save, Edit3 } from 'lucide-react';
 import styles from './Map.module.css';
 
+// Fix for default marker icon in Leaflet with webpack
+import L from 'leaflet';
+delete L.Icon.Default.prototype['_getIconUrl'];
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 // Component to handle map clicks for drawing
 function DrawingHandler({isDrawing, onAddPoint}) {
     useMapEvents({
